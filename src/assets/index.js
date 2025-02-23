a7c8c50b9e9d97d26a16b6bbf5838240cecff957
alex aku <vixndotcom@gmail.com>
2025-02-23 12:02:08 +0100

    business

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,10 +10,12 @@ import earn from './earn.png'
 import products from './products.png'
 import protect from './protect.png'
 import wealth from './wealth.png'
+import business from './business.png'
+import world from './world.png'
 
 export{
     beach,
-    earn, build, products, protect, wealth,
+    earn, build, products, protect, wealth, business, world,
 tick,
 open,
 good,

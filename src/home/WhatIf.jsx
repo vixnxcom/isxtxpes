2e5dec99e7035b77ae8561e9e05070527334131a
alex aku <vixndotcom@gmail.com>
2025-02-26 01:36:34 +0100

    border

--- src/home/WhatIf.jsx
+++ src/home/WhatIf.jsx
@@ -6,11 +6,11 @@ const WhatIf = () => {
        
     <section className={`${styles.flexStart} ${styles.paddingX} flex flex-col `}>
 
-         <div className={`${styles.heading7} mt-5`}>
+         <div className={`${styles.subHeadingy} mx-auto mt-5`}>
          What if I told you that between <br /> <span className='gold'>₦3 million</span> to <span className='gold'>₦15 million</span>  is just waiting to be found?
             </div>
 
-         <div className={`${styles.heading7} mt-2`}>
+         <div className={`${styles.subHeadingy} mt-2`}>
        Whats if, in the next <span className='aeon-bold text-[24px gold'> 
          90 days </span> you could recover everything you’ve ever lost—financially, emotionally, and mentally?
          </div>

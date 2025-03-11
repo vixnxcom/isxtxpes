273d8b83611d49ad231cdc0172bad5f01db46eb6
alex aku <vixndotcom@gmail.com>
2025-03-11 16:11:24 +0100

    forms

--- src/access/AccessForm.jsx
+++ src/access/AccessForm.jsx
@@ -4,8 +4,8 @@ const AccessForm = () => {
   return (
     <div className="w-full bg-bluee flex justify-center mt-20">
       <iframe
-       src="https://forms.zohopublic.com/vixndotcomgm1/form/TheDiamondProjectSilverPack/formperma/nzB2zIDNcE07y3drzZ3x_ce2QWmf6gl79il2XPuJbpg"
-        width="94%"
+       src="https://forms.zohopublic.com/thediamondproject888gm1/form/AccessForms/formperma/6HnL7s_wGHP9zJga4VAZ04aqWqXmeMHQpKfByKtKhus"
+        width="98%"
         height="2000px"
         frameBorder="0"
         title="Zoho Interest Form"
--- src/sample/Sampleform.jsx
+++ src/sample/Sampleform.jsx
@@ -4,8 +4,8 @@ const Sampleform = () => {
   return (
     <div className="w-full bg-bluee flex justify-center mt-20">
       <iframe
-       src="https://forms.zohopublic.com/vixndotcomgm1/form/TheDiamondProjectSilverPack/formperma/nzB2zIDNcE07y3drzZ3x_ce2QWmf6gl79il2XPuJbpg"
-        width="94%"
+       src="https://forms.zohopublic.com/thediamondproject888gm1/form/SampleForm/formperma/-Th5ZwNyIaC4wsm4_N3U5cwtngw81_jLdBWkiTcTKT4"
+        width="98%"
         height="2000px"
         frameBorder="0"
         title="Zoho Interest Form"
